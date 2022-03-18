c3848ec81c107f12a072dd79dccf0dae2c72d14f
“Karthick-Ramesh-16” <karthick.ramesh@freshworks.com>
2022-03-18 20:12:49 +0530

    Add styles to the pages

--- cart.js
+++ cart.js
@@ -1,28 +1,38 @@
+let totalAmount = 0;
 let foodItems = { "Idly" : 40, "Dosa" : 30, "Poori" : 20 };
 let cartItems = document.getElementById("cart-items");
 let items = sessionStorage.getItem("cart-items");
 if (items !== null && items !== "") {
     items = items.split(",");
-    let totalAmount = 0;
     for (let item of items) {
+        let img = document.createElement("img");
+        img.setAttribute("src", `${item}.jpeg`);
+        img.setAttribute("alt", item);
+        img.setAttribute("class", "food-item-img");
+
         let itemName = document.createElement("span");
-        itemName.appendChild(document.createTextNode(item));
-        
-        let itemPrice = document.createElement("span");
-        itemPrice.appendChild(document.createTextNode("Rs. " + foodItems[item]));
-        totalAmount += foodItems[item];
-        
+        itemName.appendChild(document.createTextNode(`${item} Rs. ${foodItems[item]}`));
+
         let button = document.createElement("button");
         button.appendChild(document.createTextNode("Remove item"));
+        button.setAttribute("class", "remove-item");
         button.setAttribute("onclick", `removeItem("${item}")`)
-        
+
+        let div = document.createElement("div");
+        div.appendChild(itemName); div.appendChild(button);
+
+        let label = document.createElement("label");
+        label.setAttribute("class", "food-item");
+        label.appendChild(img); label.appendChild(div);
+
         let li = document.createElement("li");
-        li.setAttribute("id", item);
-        li.appendChild(itemName).appendChild(itemPrice).appendChild(button);
+        li.setAttribute("id", item); li.appendChild(label);
         cartItems.appendChild(li);
     }
-    document.getElementById("total-amount").innerHTML = `Rs. ${totalAmount}`;
 }
+
+document.getElementById("total-amount").innerHTML = `Rs. ${totalAmount}`;
+
 
 function removeItem(item) {
     let cartItems = sessionStorage.getItem("cart-items");
@@ -35,4 +45,10 @@ function removeItem(item) {
             location.reload();
         }
     }
+}
+
+
+function clearCart() {
+    sessionStorage.removeItem("cart-items");
+    location.reload();
 }
--- my-cafeteria.js
+++ my-cafeteria.js
@@ -1,27 +1,5 @@
 document.getElementById("username").innerHTML = `Welcome ${sessionStorage.getItem("name")}!`;
 
-let foodItems = { "Idly" : 40, "Dosa" : 30, "Poori" : 20 }
-let items = Object.keys(foodItems);
-
-for (const item of items) {
-    let itemName = document.createElement("span");
-    itemName.appendChild(document.createTextNode(item));
-
-    let itemPrice = document.createElement("span");
-    itemPrice.appendChild(document.createTextNode("Rs. " + foodItems[item]));
-
-    let addToCartButton = document.createElement("button");
-    addToCartButton.appendChild(document.createTextNode("Add to cart"));
-    addToCartButton.setAttribute("onclick", `addToCart("${item}")`);
-
-    let label = document.createElement("label");
-    label.appendChild(itemName).appendChild(itemPrice).appendChild(addToCartButton);
-    let li = document.createElement("li");
-    li.appendChild(label);
-    document.getElementById("food-items").appendChild(li);
-}
-
-
 function addToCart(item) {
     let cartItems = sessionStorage.getItem("cart-items");
     if (cartItems !== null && cartItems !== "") {
