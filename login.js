2219fb47321797751f74e62b51b60047ec1d5977
Karthick-Ramesh-16 <karthick.ramesh@freshworks.com>
2022-06-05 14:07:58 +0530

    Ice cream Shop

--- login.js
+++
@@ -1,8 +0,0 @@
-function login() {
-    let name = document.getElementById("name");
-    let email = document.getElementById("email");
-    let password = document.getElementById("password");
-    sessionStorage.setItem("name", name.value);
-    sessionStorage.setItem("email", email.value);
-    sessionStorage.setItem("password", password.value);
-}
